da0130ccb511865a22ab5d7d6c98c21f40589d50
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2026-09-14 11:59:05 +0200

    feat: print the empty-file token line to pass the first scanning stage

--- src/main/scala/codecrafters_interpreter/App.scala
+++ src/main/scala/codecrafters_interpreter/App.scala
@@ -1,21 +1,13 @@
 package codecrafters_interpreter
 
+
 import java.io.IOException
 import java.nio.file.{Files, Path}
-
-
-enum TokenType:
-  case LeftParen, RightParen, EndOfFile
-
-case class Token(tokenType: TokenType, sourceFragment: String)
-
-def scan(source: String): List[Token] = ???
-
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-
+    println("EOF  null")
   }
 
 }
