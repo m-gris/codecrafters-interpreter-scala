f486de836f85ff6cbfa652eb70b9da4050f780bd
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2026-09-15 10:40:25 +0200

    feat: wire main to scan and render
    read the file, scan it, print one line per token.
    brittle: no arg check, no error handling, throws on bad input.

--- src/main/scala/codecrafters_interpreter/App.scala
+++ src/main/scala/codecrafters_interpreter/App.scala
@@ -8,6 +8,11 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     println("EOF  null")
+    // HUGLY & BRITTLE ... but work for now.
+    val filename = args(1)
+    val fileContent: String = Files.readString(Path.of(filename))
+    val rez: List[Token] = scan(Source(fileContent))
+    rez.map(_.render).foreach(println)
   }
 
 }
